7bd2ca1468072a37091b68b6b3a2118d560c4cf0
Alex M <alex@mosstech.io>
2020-03-23 18:03:51 +0000

    Set up CI deployment

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,65 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-if [[ -z ${NAMESPACE} ]]; then echo "Must export NAMESPACE to deploy into"; exit 1; fi
-if [[ -z ${APP_HOSTNAME} ]]; then echo "Must export APP_HOSTNAME for external access"; exit 1; fi
+function deploy() {
 
-echo "Specify username for login:"
-read username
-htpasswd -c ./auth $username
+  _assert_variables_set USERNAME PASSWORD HOSTNAME
 
-kubectl delete secret basic-auth -n=${NAMESPACE}
-kubectl create secret generic basic-auth -n=${NAMESPACE} --from-file=./auth
-rm -f ./auth
+  pushd $(dirname $BASH_SOURCE[0]) >/dev/null
 
-cat ./k8s/*.yaml | envsubst | kubectl apply -n=${NAMESPACE} -f -
+  # when running in CI, we need to set up gcloud/kubeconfig
+  if [[ ${DRONE:-} == "true" ]]; then
+    _assert_variables_set K8S_DEPLOYER_CREDS K8S_CLUSTER_NAME GCP_PROJECT_ID
+    _console_msg "-> Authenticating with GCloud"
+    echo "${K8S_DEPLOYER_CREDS}" | gcloud auth activate-service-account --key-file -
+    region=$(gcloud container clusters list --project=${GCP_PROJECT_ID} --filter "NAME=${K8S_CLUSTER_NAME}" --format "value(zone)")
+    _console_msg "-> Authenticating to cluster ${K8S_CLUSTER_NAME} in project ${GCP_PROJECT_ID} in ${region}"
+    gcloud container clusters get-credentials ${K8S_CLUSTER_NAME} --project=${GCP_PROJECT_ID} --region=${region}
+  fi
+
+  popd >/dev/null
+
+  pushd "k8s/" >/dev/null
+
+  _console_msg "Creating username/password"
+  htpasswd -bc ./auth ${USERNAME} ${PASSWORD}
+
+  _console_msg "Applying Kubernetes yaml"
+  kustomize build . | envsubst | kubectl apply -f -
+  kubectl rollout status deploy/kube-ops-view -n=mw-platform
+  
+  popd >/dev/null
+
+}
+
+function _assert_variables_set() {
+  local error=0
+  local varname
+  for varname in "$@"; do
+    if [[ -z "${!varname-}" ]]; then
+      echo "${varname} must be set" >&2
+      error=1
+    fi
+  done
+  if [[ ${error} = 1 ]]; then
+    exit 1
+  fi
+}
+
+function _console_msg() {
+  local msg=${1}
+  local level=${2:-}
+  local ts=${3:-}
+  if [[ -z ${level} ]]; then level=INFO; fi
+  if [[ -n ${ts} ]]; then ts=" [$(date +"%Y-%m-%d %H:%M")]"; fi
+  echo
+  if [[ ${level} == "ERROR" ]] || [[ ${level} == "CRIT" ]] || [[ ${level} == "FATAL" ]]; then
+    (echo 2>&1)
+    (echo >&2 "-> [${level}]${ts} ${msg}")
+  else 
+    (echo "-> [${level}]${ts} ${msg}")
+  fi
+  echo
+}
+
+deploy
